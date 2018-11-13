fb145933273250dc85c3e49cf0251ee293cf25be
mikekos7 <44125229+mikekos7@users.noreply.github.com>
2018-11-13 19:10:16 +0200

    ΝΕΑ ΕΚΔΟΣΗ ΤΟΥ ΜΕΝΟΥ
    ΠΡΟΣΘΗΚΗ ΤΗΣ ΜΕΘΟΔΟΥ ΤΕΡΜΑΤΙΣΜΟΥ ΚΑΙ ΔΙΟΡΘΩΣΗ ΤΩΝ ΕΛΛΗΝΙΚΩΝ

--- Menu.java
+++ Menu.java
@@ -3,32 +3,54 @@ public class Menu {
 	public void Select() {
 		Scanner input = new Scanner(System.in);
 		System.out.println("\t \t \t \t << MENU >>");
-		System.out.println(" Παρακαλώ  πατήστε τον αριθμό της επιλογής σας.");
-		System.out.println("<1> Ορισμός νέου πεδίου");
-		System.out.println("<2> Εισαγωγή δεδομένων");
-		System.out.println("<3> Παρουσίαση δεδομένων");
-		System.out.println("<4> Διαγραφή δεδομένων");
-		System.out.println("<5> Αλλαγή δεδομένων");
-		System.out.println("<6> Αποθήκευση δεδομένων σε αρχείο");
-		System.out.println("<7> Έξοδος!" );
+		System.out.println(" Please type the number of your selection :");
+		System.out.println("<1> Enter a new field");
+		System.out.println("<2> Data Input");
+		System.out.println("<3> Data Presentation");
+		System.out.println("<4> Delete data");
+		System.out.println("<5> Change Data");
+		System.out.println("<6> SAVE");
+		System.out.println("<7> EXIT!" );
 		int ans = input.nextInt();
 		while ( ans < 1 || ans > 7 ) {
-			System.out.println("Μη έγκυρη επιλογή , προσπαθήστε ξανά!");
+			System.out.println("Invalid number, Please try again!");
 			ans = input.nextInt();
 		}
-		//ΠΕΡΙΠΤΩΣΗ ΟΠΟΥ ΘΕΛΟΥΜΕ ΕΞΟΔΟ
-		if (ans == 7) {
-			System.out.println(" ΤΕΡΜΑΤΙΣΜΟΣ; \n Πατήστε <0> για ΟΧΙ \n Πατήστε <1> για ΝΑΙ" );
+		switch (ans) {
+			case 1: // NEW FIELD
+						break;
+			case 2:// INPUT
+						break;
+			case 3:// PRESENT
+						break;
+			case 4:// DELETE
+						break;
+			case 5:// CHANGE
+						break;
+			case 6:// SAVE
+						break;
+			case 7: exit();
+						break;
+		}
+}
+
+
+		//EXIT METHOD
+		public  void exit() {
+			Scanner input = new Scanner(System.in);
+			System.out.println(" EXIT; \n PRESS <0> --> NO \n PRESS <1> --> YES" );
 			int exit = input.nextInt();
 			while (exit != 0 && exit != 1) {
-				System.out.println(" Μη έγκυρη επιλογή, προσπαθήστε ξανά ");
-				if ( exit == 1) {
-					break;
-				} else {
+				System.out.println("Invalid number, Please try again! ");
+				exit = input.nextInt();
+			}
+			if ( exit == 1) {
+				System.exit(0);
+			} else {
 					Select();
-				}
 			}
 		}
 
-	}
+
+
 }
