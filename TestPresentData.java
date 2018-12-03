d4b3172e352680cfe647dfe5160da6364fcb97bb
RaniaVlachou <44160860+RaniaVlachou@users.noreply.github.com>
2018-12-03 17:43:24 +0200

    Add files via upload

---
+++ TestPresentData.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class TestPresentData {
+	public static void main(String[]args) {
+		presentTable();
+	}
+	public  void presentTable(LinkedList<pd> my_list) {
+			//χρηση set ωστε να μην εμφανιζονται ιδια δεδομενα που εχουν εισαχθει πανω απο μια φορα
+			Set<pd> s = new HashSet<>();
+			int i = 0;
+			for(pd data : my_list) {
+				i++;
+				if( i == 1) {
+					System.out.println(data.getType());
+					System.out.println();
+				}
+				if(s.add(data) == true)
+				System.out.println(data);
+				System.out.println(pd.type);
+			}
+		}
+}
