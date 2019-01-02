3fb00537969e756913bfa0e1f3d2d7b521b71fff
goten16 <44161360+goten16@users.noreply.github.com>
2019-01-02 17:14:43 +0200

    Set Fields Class(Κλάση Ορισμού Πεδίων)

---
+++ Set.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Set {
+	//The method returns the outer arraylist that contains the other arraylists
+	public ArrayList<ArrayList<String>> SetFields() {
+
+		boolean loop = true;
+		//The first field must be inserted at any rate
+		System.out.println("Insert a field: ");
+		ArrayList<ArrayList<String>> outer = new ArrayList<ArrayList<String>>();
+		ArrayList<String> inner = new ArrayList<String>();
+		Scanner s = new Scanner(System.in);
+		String field = s.nextLine();
+		inner.add(field);
+		outer.add(inner);
+		int counter = 0;
+		int k;
+		/*While the user wants to enter new fields the loop will continue to run,
+		it will stop when the loop variable becomes false*/
+		do {
+			System.out.println("Would you like to insert another field: 1.Yes\n2.No");
+			k = s.nextInt();
+			if (k == 1) {
+				System.out.println("Insert field's data: ");
+				inner = new ArrayList<String>();
+				field=s.nextLine();
+				inner.add(field);
+				counter++;
+				outer.add(inner);
+			} else {
+				loop = false;
+			}
+		} while(loop == true);
+		System.out.println("Field insertion stopped");
+		return outer;
+	}
+}
