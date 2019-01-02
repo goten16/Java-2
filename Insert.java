cfbc7e902d24465af3d320747fb2529a5ce3f582
goten16 <44161360+goten16@users.noreply.github.com>
2019-01-02 22:51:24 +0200

    Τελική Insert με σχόλια
    Όλες οι κλάσεις θα βασιστούν πάνω σε αυτή

--- Insert.java
+++ Insert.java
@@ -1,34 +1,62 @@
 import java.util.*;
 
-public class Insert <T> {
+public class Insert {
 
-	//Asks the user how many data he would like to insert
-	public static int HowMany() {
-		Scanner input = new Scanner(System.in);
-		System.out.println("How many data would you like to insert?");
-		int x = input.nextInt();
-		return x;
-	}
-
-
-
-	/*Data input by the user,where they are added LinkedList,parameter x
-	is the number of data from HowMany*/
-	public static LinkedList<String> DataInput(int x) {
-		Scanner input = new Scanner(System.in);
+	//Method that inserts the data into the inner arraylists and the returns it
+	public ArrayList<ArrayList<String>> InsertData(ArrayList<ArrayList<String>> outer) {
+		Scanner s = new Scanner(System.in);
+		String field;
 		String data;
-		String[] array1 = new String[x];
-		LinkedList<String> list = new LinkedList<String>();
-		for(int i=0; i < x;i++) {
-			System.out.printf("Please insert the data for position No.%d?\n", i+1);
-			data = input.nextLine();
-			array1[i] = data;
-		}
-		for (int y = 0; y < array1.length; y++){
-			list.add(y, array1[y]);
-		}
-		return list;
+		boolean b1;
+		int i;
+		int ans1;
+		int ans2;
+
+		//The first loop is for how many fields the user wants to insert data into
+		do{
+			b1 = false;
+			i=-1;
+			/*The second is for the program to see if there is the name
+				of the field in the arraylist and then take the number of its position*/
+
+			do {
+				System.out.println("Please type the field name of the data you would like to insert: ");
+				field = s.nextLine();
+
+				/*The third looks in the arraylist until it finds the name
+				of the field or the counter becomes greater than the arraylist size*/
+				do {
+					i++;
+					if (outer.get(i).get(i) == field) {
+						b1 = true;
+					}
+				} while(b1 = false || i<outer.size());
+				if (b1 = false) {
+					i = -1;
+					System.out.println("Field name not found! Please try again.");
+				} else {
+					System.out.println("Field name found!");
+				}
+			} while(b1 == false);
+
+			/*The fourth is for the number of data the user wants
+			to insert in the certain field he typed before*/
+			do {
+				System.out.println("Please insert data for the certain field: ");
+				data = s.nextLine();
+				outer.get(i).add(data);
+				System.out.println("Would you like to insert more data: 1.Yes\n2.No ");
+				ans2 = s.nextInt();
+			} while (ans2 == 1);
+			System.out.println("Would you like to insert data for another field: 1.Yes\n2.No ");
+			ans1= s.nextInt();
+		} while (ans1 == 1);
+		return outer;
 	}
 }
+
+
+
+
 
 
