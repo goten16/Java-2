c709e09345d41e230e042efbff5310c01a99e7b4
RaniaVlachou <44160860+RaniaVlachou@users.noreply.github.com>
2019-01-08 17:46:32 +0200

    Add files via upload

--- Txt.java
+++ Txt.java
@@ -4,15 +4,17 @@ import java.io.IOException;
 
 public class Txt {
 	// We create each ArrayList item in a single line to txt
-	private void createFile(String file, ArrayList<String> arrData) throws IOException {
+	private void createFile(String file, ArrayList<ArrayList<String>> outer) throws IOException {
 		FileWriter writer = new FileWriter(file + ".txt");
-		int size = arrData.size();
+		int size = outer.size();
 		for (int i = 0; i < size; i++) {
-			String str = arrData.get(i).toString();
+			for (int j = 0; j < outer.get(i).size(); j++) {
+			String str = outer.get(i).get(j).toString();
 			writer.write(str);
 			// With this we prevent creating a blank like at the end of the file
 			if (i < size - 1)
 				writer.write("\n");
+			}
 		}
 		writer.close();
 	}
